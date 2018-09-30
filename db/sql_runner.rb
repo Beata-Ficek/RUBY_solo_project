e7e32da2a0b1649cab5518e6a5ce695d67a1482f
Bea <beata.ficek@gmail.com>
2018-09-30 17:09:24 +0100

    connect to db via uri

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,11 +1,17 @@
 require('pg')
+require('uri')
+
 
 class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'save_the_bees', host: ENV["DATABASE_URL"]})
+      uri = URI.parse(ENV['DATABASE_URL'])
+
+      db = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+
       db.prepare("query", sql)
+
       result = db.exec_prepared("query", values)
     ensure
       db.close() if db != nil
